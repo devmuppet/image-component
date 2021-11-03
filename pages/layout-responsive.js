407a1030a45eb2d90439ecebe9e8aace2ddf1974
devmuppet <aidanspoopyhead@gmail.com>
2021-11-03 22:33:20 +0000

    container div

--- pages/layout-responsive.js
+++ pages/layout-responsive.js
@@ -3,13 +3,16 @@ import ViewSource from '../components/view-source'
 import mountains from '../public/IMG-20211103-WA0012.jpg'
 
 const Responsive = () => (
-  <div>
+
+  <div className={styles.container}>
+  <div className={styles.card}>
     <ViewSource pathname="pages/layout-responsive.js" />
     <Image
       alt="ewergot"
       src={mountains}
       layout="responsive"
     />
+  </div>
   </div>
 )
 
